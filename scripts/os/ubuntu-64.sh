6d7ce4a782eb78c6bab8c93658c1ecc9bbfe0dee
Chris Schaefer <chris@dtzq.com>
2015-02-05 20:34:41 -0500

    Update jdk6 to jdk7

--- scripts/os/ubuntu-64.sh
+++ scripts/os/ubuntu-64.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-APT_PACKAGES=(build-essential openjdk-6-jdk python-dev python-boto libcurl4-nss-dev libsasl2-dev maven libapr1-dev libsvn-dev cgroup-lite golang git)
+APT_PACKAGES=(build-essential openjdk-7-jdk python-dev python-boto libcurl4-nss-dev libsasl2-dev maven libapr1-dev libsvn-dev cgroup-lite golang git)
 
 install_os_deps() {
   do_apt_update
